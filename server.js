dfd979da7f96a725f76e3774faf39fc0528cf08d
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-07-20 09:19:46 -0400

    Server finalized

--- server.js
+++ server.js
@@ -1,56 +1,14 @@
-let express = require('express');
-let router = express.Router();
-let nodemailer = require('nodemailer');
-let cors = require('cors');
-const creds = require('./config');
+const express = require('express');
+const path=require("path");
+const port= process.env.PORT || 5000
 
-let transport = {
-    host: 'smtp.example.com', // Don’t forget to replace with the SMTP host of your provider
-    port: 587,
-    auth: {
-    user: creds.USER,
-    pass: creds.PASS
-  }
-}
+const app= express()
 
-let transporter = nodemailer.createTransport(transport)
+app.use(express.static(path.resolve("./client/build/"))) //Serves from the build folder
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Server is ready to take messages');
-  }
-});
+app.get("*", (req, res)=>{
+  res.sendFile("index.html")
+} )
 
-router.post('/send', (req, res, next) => {
- let name = req.body.name
-  let email = req.body.email
-  let message = req.body.message
-  let content = `name: ${name} \n email: ${email} \n message: ${message} `
+app.listen(port)
 
- let mail = {
-    from: name,
-    to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  // Change to email address that you want to receive messages on
-    subject: 'New Message from Contact Form',
-    text: content
-  }
-
-  transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        status: 'fail'
-      })
-    } else {
-      res.json({
-       status: 'success'
-      })
-    }
-  })
-})
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
-app.listen(3000)
